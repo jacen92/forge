1abd60af1cd8c439c7e9436257409bf4bf4275a9
Jacen <jacen92@gmail.com>
2019-09-08 22:28:22 +0200

    factorize admin name as much as possible in all services configurations

--- roles/infra_storage/files/nexus/bypass_wizard.groovy
+++ roles/infra_storage/files/nexus/bypass_wizard.groovy
@@ -5,5 +5,5 @@ parsed_args = new JsonSlurper().parseText(args)
 security.setAnonymousAccess(false)
 log.info('Anonymous access disabled')
 
-security.securitySystem.changePassword('admin', parsed_args.admin_password)
+security.securitySystem.changePassword(parsed_args.admin_name, parsed_args.admin_password)
 log.info('default password for admin changed')
